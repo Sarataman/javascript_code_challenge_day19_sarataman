40f24e01601709f7e7a49518d4cd740c80f27922
ehimenmen_ataman <45874171+Sarataman@users.noreply.github.com>
2020-05-21 15:37:43 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+ class Series {
+    constructor(input) {
+      this.input = input
+      this.inputArray = this.input.split('').map(a => Number(a))
+    }
+  //type code here
+  
+  }
+
+  module.exports = Series;
